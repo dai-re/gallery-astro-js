ed85413f2379cf126e98f4ba7eb349ad9e4da33d
dai-re <dairewahandi@gmail.com>
2024-08-02 09:29:34 +0700

    ⚙️: empati

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  integrations: [tailwind(), icon()]
+});
