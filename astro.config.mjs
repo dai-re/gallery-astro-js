b1dabfcee88dbc73f5d5e5bc78f2b7c73573f8f5
dai-re <dairewahandi@gmail.com>
2024-08-04 08:59:51 +0700

    🚀: add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  adapter: vercel()
 });
