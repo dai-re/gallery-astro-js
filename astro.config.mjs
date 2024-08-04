0361635762dc63501ffba4155ad9255e7e8c998b
dai-re <dairewahandi@gmail.com>
2024-08-04 13:43:37 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: 'hybrid',
   integrations: [tailwind(), icon()],
   adapter: vercel()
 });
